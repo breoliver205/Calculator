6ce5a63f9e7c03a16a1816a276e7b1167166a0aa
Trillnonymous <trillnonymous@gmail.com>
2019-12-16 23:51:06 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    $basic_functions = [
+        [7, 8, 9, (object) ["/" => "÷"]],
+        [4, 5, 6, (object) ["*" => "×"]],
+        [1, 2, 3, "-"],
+        [(object) ["neg" => "±" ], 0, ".", "+"]
+    ];
+
+    $calc_functions = [
+        ["2nd", "sin", "ln"],
+        [(object) ["pi" => ""], "cos", "log"],
+        ["e", "tan", (object) ["sqrt" => ""]],
+        ["Ans", "exp", "pow"]
+    ];
+?>
